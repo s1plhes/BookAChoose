46236a3db2b044c5391b851d8733d5d7dd71f464
Siplhes <nsfwmcbo@gmail.com>
2024-08-31 02:19:12 -0400

    WTF

--- dist/assets/checkAuth-4erMPWyQ.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-vyn2smVK.js";const s=o=>{const t=e();o.value||t.push("/403")};export{s as c};
---
+++ dist/assets/checkAuth-BEeJuQFR.js
@@ -0,0 +1,1 @@
+import{z as e}from"./index-BFBO9Br6.js";const s=o=>{const t=e();o.value||t.push("/403")};export{s as c};
